92ddcd285a3178160bd0fd445469ee6572956ba3
heroxdream <heroxdream@gmail.com>
2015-08-26 12:55:09 -0400

    longestPalindrome Manacher'a algorithm

--- src/string/longestPalindrome.py
+++ src/string/longestPalindrome.py
@@ -117,6 +117,47 @@ def dplongestPalindromeV5(s):
     return s[bstart:bend + 1]
 
 def linerLongestPalindromeV6(s):
+    """
+    string(i,l) is a substring of s where i is the start index and l is the length
+
+    S(n) is the longest palindrome for substring of s with indice from 0 to n
+
+    if S(n-1) = string(i,l)
+
+    then S(n) =
+
+    string(n-l, l+1) if string(n-l, l+1) is palindrome
+    string(n-l-1, l+2) if string(n-l-1, l+2) is palindrome
+    S(n) otherwise
+
+    # The key intuition of this algorithm is that palindromes are made up of
+    # smaller palindromes.
+
+    # So, a palindrome of length 100 (for example), will have a palindrome of
+    # length 98 inside it, and one of length 96, ... 50, ... 4, and 2.
+
+    # Because of this, we can move across our string, checking if the current
+    # place is a palindrome of a particular length (the longest length palindrome
+    # found so far + 1), and if it is, update the longest length, and move forward.
+
+    # In this way, we find our longest palindromes "from the inside out", starting
+    # with length x, then x+2, x+4, ...
+
+    # Example:
+    # "xxABCDCBAio"
+    #  0123456789  < indexes
+    # As we scan our string, we initially find a palindrome of length 2 (xx)
+    # We always look backwards!
+    # When we get to index 2,3,4, we see no length 3+ palindrome ending there.
+    # But when we get to index 6, looking back 3 characters, we see "CDC"! So our
+    # longest palindrome is now length 3.
+    # At index 7, we look back and see no length 4 palindromes, but find one of
+    # length 5 ("BCDCB").
+    # And finally, by i = 8, we find the full "ABCDCBA"
+
+    :param s:
+    :return:
+    """
 
     start = 0
     max_len = 0
@@ -129,6 +170,27 @@ def linerLongestPalindromeV6(s):
             max_len += 1
     return s[start:start + max_len]
 
+
+def manacherLongestPalindrome(s):
+
+    t = '#'.join('^{}$'.format(s))
+    right = 0
+    center = 0
+    p = [0] * len(t)
+    for i in range(1, len(t) - 1):
+
+        p[i] = int(right > i) and min(p[2 * center - i], right - i)
+
+        while t[i - p[i] - 1] == t[i + p[i] + 1]:
+            p[i] += 1
+
+        if i + p[i] > right:
+            center, right = i, i + p[i]
+
+    max_len, best_center = max((n, i) for i, n in enumerate(p))
+
+    return s[(best_center - max_len) // 2: (best_center + max_len) // 2]
+
 if __name__ == '__main__':
 
     s0 = 'a'
@@ -136,6 +198,7 @@ if __name__ == '__main__':
     s2 = 'abac'
     s3 = 'ibvjkmpyzsifuxcabqqpahjdeuzaybqsrsmbfplxycsafogotliyvhxjtkrbzqxlyfwujzhkdafhebvsdhkkdbhlhmaoxmbkqiwiusngkbdhlvxdyvnjrzvxmukvdfobzlmvnbnilnsyrgoygfdzjlymhprcpxsnxpcafctikxxybcusgjwmfklkffehbvlhvxfiddznwumxosomfbgxoruoqrhezgsgidgcfzbtdftjxeahriirqgxbhicoxavquhbkaomrroghdnfkknyigsluqebaqrtcwgmlnvmxoagisdmsokeznjsnwpxygjjptvyjjkbmkxvlivinmpnpxgmmorkasebngirckqcawgevljplkkgextudqaodwqmfljljhrujoerycoojwwgtklypicgkyaboqjfivbeqdlonxeidgxsyzugkntoevwfuxovazcyayvwbcqswzhytlmtmrtwpikgacnpkbwgfmpavzyjoxughwhvlsxsgttbcyrlkaarngeoaldsdtjncivhcfsaohmdhgbwkuemcembmlwbwquxfaiukoqvzmgoeppieztdacvwngbkcxknbytvztodbfnjhbtwpjlzuajnlzfmmujhcggpdcwdquutdiubgcvnxvgspmfumeqrofewynizvynavjzkbpkuxxvkjujectdyfwygnfsukvzflcuxxzvxzravzznpxttduajhbsyiywpqunnarabcroljwcbdydagachbobkcvudkoddldaucwruobfylfhyvjuynjrosxczgjwudpxaqwnboxgxybnngxxhibesiaxkicinikzzmonftqkcudlzfzutplbycejmkpxcygsafzkgudy'
     s4 = 'cccc'
+    s5 = 'bb'
 
     from time import time
 
@@ -190,13 +253,23 @@ if __name__ == '__main__':
     # print(t2 - t1)
     # print(dplongestPalindromeV5(s4))
 
-    print(linerLongestPalindromeV6(s0))
-    print(linerLongestPalindromeV6(s1))
-    print(linerLongestPalindromeV6(s2))
+    # print(linerLongestPalindromeV6(s0))
+    # print(linerLongestPalindromeV6(s1))
+    # print(linerLongestPalindromeV6(s2))
+    #
+    # t1 = time() * 1000
+    # print(linerLongestPalindromeV6(s3))  # 1.20 ms, good
+    # t2 = time() * 1000
+    # print(t2 - t1)
+    # print(linerLongestPalindromeV6(s4))
+
+    print(manacherLongestPalindrome(s0))
+    print(manacherLongestPalindrome(s1))
+    print(manacherLongestPalindrome(s2))
 
     t1 = time() * 1000
-    print(linerLongestPalindromeV6(s3))  # 1.20 ms, good
+    print(manacherLongestPalindrome(s3))  # 2 ms, good
     t2 = time() * 1000
     print(t2 - t1)
-    print(linerLongestPalindromeV6(s4))
-
+    print(manacherLongestPalindrome(s4))
+    print(manacherLongestPalindrome('a'))
