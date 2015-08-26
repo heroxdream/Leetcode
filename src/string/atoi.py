f9f64831fb19242793f6167a21ad058629b32a4c
heroxdream <heroxdream@gmail.com>
2015-08-26 15:24:20 -0400

    atoi

--- src/string/atoi.py
+++ src/string/atoi.py
@@ -1,17 +1,34 @@
 __author__ = 'hanxuan'
 def atoi(sx):
+    """
+    :param sx: string
+    :return: int
+
+    lots of corner cases:
+    '+1234'
+    '-1234'
+    '1234'
+    '+1234abc'
+    '   1234'
+    '000123'
+    ' - 0001234'
+    '12341234123412341234123412341341'      too big
+    '-12341234123412341234123412341341'     too small overflow
+
+    """
     import re
 
-    MAX_INT = pow(2, 31) - 1
-    MIN_INT = - pow(2, 31)
+    max_int = pow(2, 31) - 1
+    min_int = - pow(2, 31)
 
     sx = sx.strip()
     valid_sx = re.findall('(^\+|\-\d*)\D*', sx)
-    print(valid_sx)
+
     try:
         result = int(''.join(valid_sx))
-        return result if result in range(MIN_INT, MAX_INT + 1) else MAX_INT if result > MAX_INT else MIN_INT
-    except:
+        return result if result in range(min_int, max_int + 1) else max_int if result > max_int else min_int
+    except Exception as e:
+        print(e)
         return 0
 
 if __name__ == '__main__':
