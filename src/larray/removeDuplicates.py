42232fe4c8c54a4ab0290613988855f2c5f5d638
heroxdream <heroxdream@gmail.com>
2015-09-29 17:20:25 -0400

    remove duplicates in sorted array version 2, duplicates allowed

--- src/larray/removeDuplicates.py
+++ src/larray/removeDuplicates.py
@@ -28,9 +28,50 @@ def removeDuplicates(nums):
 
     return n
 
+def removeDuplicates_v2(nums):
+    """
+    :param nums:
+    :return:
+    What if duplicates are allowed at most twice?
+
+    For example,
+    Given sorted array nums = [1,1,1,2,2,3],
+    Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3.
+    It doesn't matter what you leave beyond the new length.
+    """
+
+    if len(nums) <= 2:
+        return len(nums)
+
+    p = 0
+    n = len(nums)
+    twice = False
+    for i in range(1, len(nums)):
+        if nums[i] == nums[p] and not twice:
+            nums[p + 1] = nums[i]
+            p += 1
+            twice = True
+        elif nums[i] == nums[p] and twice:
+            n -= 1
+        else:
+            nums[p + 1] = nums[i]
+            p += 1
+            twice = False
+
+    print(nums)
+    return n
+
+
+
 
 if __name__ == '__main__':
-    print(removeDuplicates([1, 2, 3, 3]))
-    print(removeDuplicates([1]))
-    print(removeDuplicates([1, 1, 2]))
-    print(removeDuplicates([]))
+    # print(removeDuplicates([1, 2, 3, 3]))
+    # print(removeDuplicates([1]))
+    # print(removeDuplicates([1, 1, 2]))
+    # print(removeDuplicates([]))
+
+    print(removeDuplicates_v2([1,1,1,2,2,3]))
+    print(removeDuplicates_v2([1,1,1,1,1,1,1]))
+    print(removeDuplicates_v2([1]))
+
+
