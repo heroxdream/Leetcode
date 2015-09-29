11cd349b3256a60f55212647c661a36cd273c41e
heroxdream <heroxdream@gmail.com>
2015-09-29 19:56:02 -0400

    delete duplicate from sorted list

--- src/llinkedList/deleteDuplicateFromSortedList.py
+++ src/llinkedList/deleteDuplicateFromSortedList.py
@@ -28,6 +28,43 @@ def deleteDuplicateFromSortedList(head):
         fast = fast.next
     return head
 
+def deleteDuplicateFromSortedListV2(head):
+    """
+    :param head:
+    :return:
+    Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+    For example,
+    Given 1->2->3->3->4->4->5, return 1->2->5.
+    Given 1->1->1->2->3, return 2->3.
+    """
+
+    if not head or not head.next:
+        return head
+
+    h = head.next
+    t = head
+    duplicates = set()
+    while h:
+        if h.val == t.val:
+            duplicates.add(h.val)
+        t = h
+        h = h.next
+
+    h = head
+    d = ListNode(0.5)
+    t = d
+    while h:
+        if h.val in duplicates:
+            t.next = None
+        else:
+            t.next = h
+            t = h
+        h = h.next
+
+    return d.next
+
+
 import unittest
 
 class TestDeleteDuplicateFromSortedList(unittest.TestCase):
@@ -41,9 +78,36 @@ class TestDeleteDuplicateFromSortedList(unittest.TestCase):
             ListNode.build_from_array([1])))
 
         self.assertTrue(ListNode.equal(
-            deleteDuplicateFromSortedList(ListNode.build_from_array([1, 1, 2, 3, 3, 3])),
+                deleteDuplicateFromSortedList(ListNode.build_from_array([1, 1, 2, 3, 3, 3])),
             ListNode.build_from_array([1, 2, 3])))
 
         self.assertTrue(ListNode.equal(
             deleteDuplicateFromSortedList(ListNode.build_from_array([])),
             ListNode.build_from_array([])))
+
+    def test2(self):
+        self.assertTrue(ListNode.equal(
+            deleteDuplicateFromSortedListV2(ListNode.build_from_array([1, 1])),
+            ListNode.build_from_array([])))
+
+        self.assertTrue(ListNode.equal(
+            deleteDuplicateFromSortedListV2(ListNode.build_from_array([1])),
+            ListNode.build_from_array([1])))
+
+        self.assertTrue(ListNode.equal(
+            deleteDuplicateFromSortedListV2(ListNode.build_from_array([1, 1, 2, 3, 3, 3])),
+            ListNode.build_from_array([2])))
+
+        self.assertTrue(ListNode.equal(
+            deleteDuplicateFromSortedListV2(ListNode.build_from_array([])),
+            ListNode.build_from_array([])))
+
+        self.assertTrue(ListNode.equal(
+            deleteDuplicateFromSortedListV2(ListNode.build_from_array([1, 2, 3, 3, 4, 4, 5])),
+            ListNode.build_from_array([1, 2, 5])))
+
+
+if __name__ == '__main__':
+    h0 = ListNode.build_from_array([1, 1])
+    h1 = deleteDuplicateFromSortedListV2(h0)
+    h1.traverse()
